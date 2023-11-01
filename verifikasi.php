eb55f7b83c386c2eb0bc3de117d355d473b6502c
HELL __ <gantenggaluh805@gmail.com>
2023-11-01 10:55:35 +0700

    penambahan lupa pass dan verifikasi

---
+++ verifikasi.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Verifikasi Email</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;300;400;700&display=swap" rel="stylesheet">
+  <link rel="icon" href="/img/soccer-ground.ico">
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="verify">
+  <div class="center">
+    <h1>Verifikasi Email</h1>
+    <form method="POST">
+      <div class="txt_field">
+        <input type="email" required>
+        <span></span>
+        <label>Email</label>
+      </div>
+      <button class="button btn-inti" name="Verify" id="verify">Verifikasi</button>
+      <div class="verify_link">
+        Kembali menuju <a href="login.php">Login</a> atau <a href="lupapass.php">Reset password</a>
+      </div>
+    </form>
+  </div>
+
+</body>
+
+</html>
