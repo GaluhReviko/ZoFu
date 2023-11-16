e6b59c29727758bf4a77442c279c7433507d434d
YukatariEka <148833947+YukatariEka@users.noreply.github.com>
2023-11-16 15:38:18 +0700

    Update login.php

--- login.php
+++ login.php
@@ -67,10 +67,10 @@ if (isset($_POST["login"])) {
         <span></span>
         <label>Password</label>
       </div>
-      <div class="reset_link"><a href="verifikasi.php">Lupa password?</a></div>
+      <div class="reset_link"><a href="verifikasi.php">Lupa Password?</a></div>
       <button class="button btn-inti" name="login" id="login">Login</button>
       <div class="signup_link">
-        Belum punya akun? <a href="user/daftar.php">Daftar</a>
+        Belum Punya Akun? <a href="user/daftar.php">Daftar</a>
       </div>
     </form>
   </div>
